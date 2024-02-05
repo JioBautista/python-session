b807dd96cf99d960d6219892c00aeec4539b6bac
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-05 16:56:28 -0500

    Update practice.py

--- server/practice.py
+++ server/practice.py
@@ -1,6 +1,26 @@
-username = "Jbautista"
-password = 2113
-email = 'pachojio@gmail.com'
+first_name = "Jio Carlo"
+first_pet = "Chewy"
+second_pet = "Choco"
+birth_date = 1111994
+first_pet_birth_date = str(4212019)
+second_pet_birth_date = 7132020
 
-print(type(password))
-print(username, password, email)
+
+isAdmin = True
+
+
+# TERNARY OPERATOR
+# print("Welcome Admin user: " +first_name) if isAdmin == True else print("Welcome User")
+
+if isAdmin == True:
+    print("Welcome Admin " +first_name.upper())
+else:
+    print("Unauthorized user")
+
+print(first_name[0:3])
+
+gpa = 4.3
+
+print(round(gpa))
+
+print(round(gpa, 1))
