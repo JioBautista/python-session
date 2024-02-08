4c587b213df6653a0b675558cf2c795e43298dab
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-08 09:59:41 -0500

    Update practice.py

--- server/practice.py
+++ server/practice.py
@@ -1,9 +1,11 @@
 import sys
 
+data = [11, "Jio", 30]
+
 users = {
     "firstName" : "Carlo",
-    "age": 21,
-    "birthday": "January 11 1994"
+    "birthday": "January 11 1994",
+    "age": 21
 }
 
 if users["firstName"] == "Jio" or users["age"] == 30:
@@ -11,12 +13,5 @@ if users["firstName"] == "Jio" or users["age"] == 30:
 else:
     print("Incorrect information")
 
-playerchoice = input("""Enter...
-     1 for Rock,
-     2 for Paper,
-     3 for Scissors: """)
 
-player = int(playerchoice)
-
-if player < 1 or player > 3:
-    sys.exit("You must enter 1,2 or 3.")
+print( 29 in data )
