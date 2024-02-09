db5f3fb3fa95dd0ee6f951425418b690c53486df
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-09 12:14:34 -0500

    Updates

--- server/practice.py
+++ server/practice.py
@@ -5,10 +5,22 @@ user = {
     "dogs": ["Chewy", "Choco"],
 }
 message = "This persons name is {} and his pets' names are {} and {}"
+updatedUser = "This persons name changed to {}"
 
-user["first_name"] = "Carlo"
 
-if user["first_name"] == "Jio":
-    print(message.format(user["first_name"], user["dogs"[0]]))
+if user["first_name"] == "Jio" and "Choco" and "Chewy" in user["dogs"]:
+    print(message.format(user["first_name"], user["dogs"][0], user["dogs"][1]))
+elif user["first_name"] == "Carlo":
+    print(updatedUser.format(user["first_name"]))
 else:
-    print("This person doesn't exist")
+    print("This user doesn't exist")
+
+newDogs = ["Kratos", "Mimir", "Tyr"]
+
+user["dogs"].insert(1, "Bambam")
+user["dogs"].append("Atreus")
+user["dogs"].extend(newDogs)
+user["dogs"].remove("Bambam")
+del user["dogs"][4]
+
+print(user["dogs"])
