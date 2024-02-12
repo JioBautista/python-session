10c30939103e64ad7860f0630b78cc5c647c45d1
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-12 10:09:51 -0500

    Update practice.py

--- server/practice.py
+++ server/practice.py
@@ -1,34 +1,21 @@
-user = {
-    "first_name": "Jio",
-    "age": 30,
-    "birthday": "January 11 1994",
-    "dogs": ["Chewy", "Choco"],
-}
-newUser = {
-    ("Jio", "Pacho"): {"age": 30, "birthday": "January 11, 1994"},
-    ("John", "Doe"): {"age": 30, "birthday": "December 11 1990"},
-}
-message = "This persons name is {} and his pets' names are {} and {}"
-updatedUser = "This persons name changed to {}"
+# THIS IS A LIST
+dogs = ["Chewy", "Choco"]
+dogs.insert(1, "Bambam")
 
+# THIS IS A TUPLE
+name = ("Jio", "Carlo")
 
-if user["first_name"] == "Jio" and "Choco" and "Chewy" in user["dogs"]:
-    print(message.format(user["first_name"], user["dogs"][0], user["dogs"][1]))
-elif user["first_name"] == "Carlo":
-    print(updatedUser.format(user["first_name"]))
-else:
-    print("This user doesn't exist")
+# THIS IS A SET
 
-newDogs = ["Kratos", "Mimir", "Tyr", "BamBam"]
+age = {28, 29, 30}
 
-user["dogs"].extend(newDogs)
-dogsCopy = user["dogs"].copy()
-dogsCopy.append("Odin")
+# STRING FORMATTING
+message = "This persons name is {}"
 
-for x in newUser:
-    if "Jio" in x:
-        print("correct")
-    elif "John" in x:
-        print("Also correct")
-    else:
-        print("Don't exist")
+# IF STATEMENT
+if "Jio" in name:
+    print(message.format(name[0]))
+
+# FOR LOOPS
+for x in dogs:
+    print(x)
