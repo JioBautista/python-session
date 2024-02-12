bd82862ed73bc1dbef3b80d152e106ed8145a3d0
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-12 14:17:59 -0500

    Update practice.py

--- server/practice.py
+++ server/practice.py
@@ -9,13 +9,30 @@ name = ("Jio", "Carlo")
 
 age = {28, 29, 30}
 
+# THIS IS A DICTIONARY
+user = {
+    "first": "Jio",
+    "last": "Pacho",
+    "age": 30,
+    "birthdate": "January 11, 1994",
+    "dogs": ["Chewy", "Choco"],
+}
+
+user.update({"dogs": ["Chewy", "Choco", "Bambam"]})
+
 # STRING FORMATTING
 message = "This persons name is {}"
 
 # IF STATEMENT
 if "Jio" in name:
     print(message.format(name[0]))
+if "dogs" in user:
+    print("Key exists")
 
 # FOR LOOPS
-for x in dogs:
-    print(x)
+for keys in user.keys():
+    print(keys)
+for values in user.values():
+    print(values)
+for items in user.items():
+    print(items)
