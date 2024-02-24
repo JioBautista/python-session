7825422d770fbee03d12c3d7efaffd01bf24ab20
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-24 15:30:13 -0500

    Update practice.py

--- server/practice.py
+++ server/practice.py
@@ -94,3 +94,24 @@ stackCalls.push("Choco")
 stackCalls.pop()
 
 print(stackCalls.items)
+
+
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, root, key):
+        if root is None:
+            return TreeNode(key)
+        if key < root.value:
+            root.left = self._insert(root.left, key)
+        else:
+            root.right = self._insert(root.right, key)
+        return root
